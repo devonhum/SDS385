777cfed3461eb67c3866440dfcc56cab366105e6
devonhum <devon.humphreys@utexas.edu>
2016-09-16 12:39:35 -0500

    final updates to linear regression code for exercise 1

--- exercises01/linear_regression.R
+++ exercises01/linear_regression.R
@@ -9,60 +9,46 @@ library(Matrix)
 
 # the matrix X contains our explanatory variables in y = Xb + e
 
-inversion_solver <- function (n_obs, n_vars) {
-  # create a random matrix for demonstration
-  X <- rnorm(n_obs * n_vars, mean = 0, sd = 1)
-  X <- matrix(X, nrow = n_obs, ncol = n_vars)
-  
-  # generate the vector y to be regressed on X
-  y <- rnorm(n_obs, mean = 0, sd = 1)
-  
-  # solve the least squares problem using the inversion method 
-  b <- solve(t(X) %*% X) %*% t(X) %*% y
-  
+inversion_solver <- function (X, y) {
+  b <- solve(t(X) %*% X) %*% t(X) %*% y # solve the least squares problem using the inversion method 
   return (b)
 }
 
 ## QR method
 
-qr_solver <- function (n_obs, n_vars) {
-  # generate a random feature matrix X
-  X <- rnorm(n_obs * n_vars, mean = 0, sd = 1)
-  X <- matrix(X, nrow = n_obs, ncol = n_vars)
-  
-  # generate an observation vector y
-  y <- rnorm(n_obs, mean = 0, sd = 1)
-  y <- matrix(y, nrow = n_obs, ncol = n_vars)
-  
-  # solve the least squares problem using QR decomposition of X
-  b <- qr.solve(X, y)
+qr_solver <- function (X, y) {
+  b <- qr.solve(X, y)  # solve the least squares problem using QR decomposition of X
   return (b)
 }
 
 ## Dealing with sparse matrices in R
-
-inversion_sparse <- function (n_obs, n_vars, sparsity) {
-  # define a random feature matrix X with sparsity of 95%
-  X <- rnorm(n_obs * n_vars, mean = 0, sd = 1)
-  X <- matrix(X, nrow = n_obs, ncol = n_vars)
-  mask <- matrix(rbinom(n_obs * n_vars, 1, sparsity), nrow = n_obs)
-  X <- X * mask
+# Begin with a dense matrix, and convert to sparse by masking with 0s.
+# Then we will demonstrate how to handle such a matrix for illustration.
+inversion_sparse <- function (X, y, sparsity) {
+  mask <- matrix(rbinom(N*P, 1, sparsity), nrow = N)
+  X <- mask * X # generate the sparsity
   X <- Matrix(X, sparse = T) # converts to sparse format
-  
-  # generate random observations in a vector y
-  y <- rnorm(n_obs, mean = 1, sd = 1)
-  
-  # solve the least squares problem taking advantage of the sparse matrix format
-  inv_mat <- solve(t(X) %*% X, sparse = T)
+  inv_mat <- solve(t(X) %*% X, sparse = T) # solve the least squares problem taking advantage of the sparse matrix format
   b <- inv_mat %*% t(X) %*% y
   return (b)
 }
 
-## benchmarking the dense matrix solvers on increasing number of variables
-var100 <- microbenchmark(inversion_solver(200, 100), qr_solver(200, 100), times = 10); var100
-var1000 <- microbenchmark(inversion_solver(2000, 1000), qr_solver(2000, 1000), times = 10); var1000
-var2000 <- microbenchmark(inversion_solver(5000, 2000), qr_solver(5000, 200), times = 10); var2000
-var5000 <- microbenchmark(inversion_solver(2000, 5000), qr_solver(2000, 5000), times = 10); var5000
+# Examples 
+N <- 5000 # note that N must be > P for the matrix to be non-singular
+P <- 1000
+X <- rnorm(N*P, mean = 0, sd = 1)
+X <- matrix(X, nrow = N, ncol = P)
+y <- rnorm(N, mean = 0, sd = 1)
+# define global sparse matrices for benchmarking
+mask <- matrix(rbinom(N*P, 1, 0.1), nrow = N)
+X_sp <- mask * X
 
-## benchmarking the sparse matrix solvers on increasing levels of sparsity and dimension
-#sp05 <- microbenchmark(inversion_sparse(150, 50), inversion_solver(150, 50), times = 10); sp05
+## benchmarking the dense matrix solvers on increasing number of variables
+var200 <- microbenchmark(inversion_solver(X, y), qr_solver(X, y), times = 10); var200
+var500 <- microbenchmark(inversion_solver(X, y), qr_solver(X, y), times = 10); var500
+var1000 <- microbenchmark(inversion_solver(X, y), qr_solver(X, y), times = 10); var1000
+var5000 <- microbenchmark(inversion_solver(X, y), qr_solver(X, y), times = 10); var5000
+
+## benchmarking the sparse matrix solvers on increasing levels of sparsity
+sp_0.05 <- microbenchmark(inversion_sparse(X, y, 0.05), inversion_solver(X_sp, y), times = 10); sp_0.05
+sp_0.1 <- microbenchmark(inversion_sparse(X, y, 0.1), inversion_solver(X_sp, y), times = 10); sp_0.1
